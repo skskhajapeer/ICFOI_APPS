db3958af922d1dbb86c31e6c1e297d962aaa19e7
Murali <mohanmca009@gamil.com>
2021-04-10 15:27:04 +0530

    Functionality Changes

--- app/src/main/java/com/roomdb/example/AddingData.kt
+++ app/src/main/java/com/roomdb/example/AddingData.kt
@@ -1,6 +1,7 @@
 package com.roomdb.example
 
 import android.os.Bundle
+import android.text.format.DateFormat
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
@@ -9,12 +10,18 @@ import com.google.gson.Gson
 import com.roomdb.example.databinding.ActivityAddDataBinding
 import com.roomdb.example.model.AddDataModel
 import com.roomdb.example.model.Sites
+import me.adawoud.bottomsheettimepicker.BottomSheetTimeRangePicker
+import me.adawoud.bottomsheettimepicker.OnTimeRangeSelectedListener
 import java.io.IOException
 import java.nio.charset.Charset
 
 
-class AddingData : AppCompatActivity() {
+class AddingData : AppCompatActivity(), OnTimeRangeSelectedListener {
     var slNumbers: ArrayList<String> = ArrayList()
+
+    private val preferenceHelper: IPreferenceHelper by lazy { PreferenceManager(applicationContext) }
+
+    private val tagBottomSheetTimeRangePicker = "tagBottomSheetTimeRangePicker"
     private lateinit var bindinggView : ActivityAddDataBinding
     private var distSpinnerValue: String? = null
 
@@ -53,6 +60,7 @@ class AddingData : AppCompatActivity() {
                 }
 
             }
+        setUIData()
     }
 
     fun setDataBasedOnSelection(siteInfo: Sites) {
@@ -71,11 +79,28 @@ class AddingData : AppCompatActivity() {
             compartments.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             bindinggView.compartmentSpinner.setAdapter(compartments)
 
-
         }else{
             bindinggView.txtCompartment.text = siteInfo.compartment
             bindinggView.compartmentSpinner.visibility = View.GONE
         }
+
+    }
+
+    fun setUIData(){
+        bindinggView.btnSelectTime.setOnClickListener( View.OnClickListener {
+            BottomSheetTimeRangePicker
+                .tabLabels(startTabLabel = "Start Time", endTabLabel = "End Time")
+                .doneButtonLabel("Ok")
+                .startTimeInitialHour(2)
+                .startTimeInitialMinute(11)
+                .endTimeInitialHour(10)
+                .endTimeInitialMinute(22)
+                .newInstance(this@AddingData, DateFormat.is24HourFormat(this))
+                .show(supportFragmentManager, tagBottomSheetTimeRangePicker)
+        })
+
+        bindinggView.etLat.setText(preferenceHelper.getUserId())
+        bindinggView.etLong.setText(preferenceHelper.getStartTimer())
 
     }
 
@@ -94,6 +119,31 @@ class AddingData : AppCompatActivity() {
             return ""
         }
         return json
+    }
+
+    override fun onTimeRangeSelected(
+        startHour: Int,
+        startMinute: Int,
+        endHour: Int,
+        endMinute: Int
+    ) {
+        var startHourString = startHour.toString()
+        var startMinuteString = startMinute.toString()
+        var endHourString = endHour.toString()
+        var endMinuteString = endMinute.toString()
+        when {
+            startHour < 9 -> startHourString = startHour.toString().prependZero()
+            startMinute < 9 -> startMinuteString = startMinute.toString().prependZero()
+            endHour < 9 -> endHourString = endHour.toString().prependZero()
+            endMinute < 9 -> endMinuteString = endMinute.toString().prependZero()
+        }
+
+        bindinggView.txtStime.text = startHourString + " " + startMinuteString
+        bindinggView.txtEndTime.text = endHourString + " " + endMinuteString
+    }
+
+    private fun String.prependZero(): String {
+        return "0".plus(this)
     }
 
 }
